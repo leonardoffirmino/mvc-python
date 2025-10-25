96b096035a7b7bcfd13fe5574af0743fc73c3b5b
Leonardo <firmino750@gmail.com>
2025-10-25 17:07:06 -0300

    feat: Implementing test repository with pytest

---
+++ src/models/sqlite/repositories/pets_repository_test.py
@@ -0,0 +1,41 @@
+from unittest import mock
+from mock_alchemy.mocking import UnifiedAlchemyMagicMock
+from src.models.sqlite.entities.pets import PetsTable
+from .pets_repository import PetsRepository
+
+class MockConnection:
+    def __init__(self):
+        self.session = UnifiedAlchemyMagicMock(
+          data=[
+            (
+              [mock.call.query(PetsTable)], #Query
+              [
+                PetsTable(name="dog",type="dog"),
+                PetsTable(name="cat", type="cat")
+              ]  #Result
+            )
+          ]
+        )
+    def __enter__(self): return self
+    def __exit__(self, exc_type, exc_val, exc_tb): pass
+
+def test_list_pets():
+    mock_connection = MockConnection()
+    repo = PetsRepository(mock_connection)
+    response = repo.list_pets()
+
+    mock_connection.session.query.assert_called_once_with(PetsTable)
+    mock_connection.session.all.assert_called_once()
+    mock_connection.session.filter.assert_not_called()
+
+
+    assert response[0].name == "dog"
+
+def test_delete_pets():
+    mock_connection = MockConnection()
+    repo = PetsRepository(mock_connection)
+    repo.delete_pets("petName")
+
+    mock_connection.session.query.assert_called_once_with(PetsTable)
+    mock_connection.session.filter.assert_called_once_with(PetsTable.name == "petName")
+    mock_connection.session.delete.assert_called_once()
